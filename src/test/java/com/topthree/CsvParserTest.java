cf6601e93c8f0fd9d4b3de81b8bec1e5bd0d9ee9
Dave <davecranesunwheel@gmail..com>
2026-06-07 20:03:53 +0100

    test(red): non-integer normalised-score returns ParseError

--- src/test/java/com/topthree/CsvParserTest.java
+++ src/test/java/com/topthree/CsvParserTest.java
@@ -39,6 +39,16 @@ class CsvParserTest {
     }
 
     @Test
+    void nonIntegerNormalisedScoreReturnsParseError() {
+        Result<ScoreRecord, ParseError> result = parser.parseLine("p1,Alice,g1,Chess,2,1.5");
+
+        assertInstanceOf(Result.Err.class, result);
+
+        ParseError error = ((Result.Err<ScoreRecord, ParseError>) result).error();
+        assertNotNull(error);
+    }
+
+    @Test
     void validSixFieldLineProducesCorrectScoreRecord() {
         Result<ScoreRecord, ParseError> result = parser.parseLine("p1,Alice,g1,Chess,2,50");
 
