940d1c0d1406ffe864b23a58ee79621d2e25df12
Dave <davecranesunwheel@gmail..com>
2026-06-07 19:57:30 +0100

    feat(green): minimal CsvParser happy path

--- src/main/java/com/topthree/DefaultCsvParser.java
+++ src/main/java/com/topthree/DefaultCsvParser.java
@@ -6,7 +6,10 @@ public class DefaultCsvParser implements CsvParser {
 
     @Override
     public Result<ScoreRecord, ParseError> parseLine(String csvLine) {
-        return null;
+        String[] fields = csvLine.split(",");
+        Player player = new Player(fields[0], fields[1]);
+        GameEntry gameEntry = new GameEntry(fields[2], fields[3], Integer.parseInt(fields[4]), Integer.parseInt(fields[5]));
+        return new Result.Ok<>(new ScoreRecord(player, gameEntry));
     }
 
     @Override
