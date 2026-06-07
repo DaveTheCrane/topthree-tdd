16230d11fd66b1047f2b1ead8a49f795cbdc5ada
Dave <davecranesunwheel@gmail..com>
2026-06-07 20:08:57 +0100

    test(red): empty player id returns ParseError

--- src/test/java/com/topthree/CsvParserTest.java
+++ src/test/java/com/topthree/CsvParserTest.java
@@ -69,6 +69,16 @@ class CsvParserTest {
     }
 
     @Test
+    void emptyPlayerIdReturnsParseError() {
+        Result<ScoreRecord, ParseError> result = parser.parseLine(",Alice,g1,Chess,2,50");
+
+        assertInstanceOf(Result.Err.class, result);
+
+        ParseError error = ((Result.Err<ScoreRecord, ParseError>) result).error();
+        assertNotNull(error);
+    }
+
+    @Test
     void validSixFieldLineProducesCorrectScoreRecord() {
         Result<ScoreRecord, ParseError> result = parser.parseLine("p1,Alice,g1,Chess,2,50");
 
