d668657c08c0c7170c6cc4f00b4b111d30aac8d1
Dave <davecranesunwheel@gmail..com>
2026-06-08 14:48:33 +0100

    test(red): empty player id returns ParseError

--- src/test/java/com/topthree/CsvParserTest.java
+++ src/test/java/com/topthree/CsvParserTest.java
@@ -85,4 +85,15 @@ class CsvParserTest {
         ParseError error = ((Result.Err<ScoreRecord, ParseError>) result).error();
         assertThat(error.offendingLine()).isEqualTo(csvLine);
     }
+
+    @Test
+    void emptyPlayerIdReturnsParseError() {
+        String csvLine = " ,Alice,g1,Chess,2,50";
+
+        Result<ScoreRecord, ParseError> result = parser.parseLine(csvLine);
+
+        assertThat(result).isInstanceOf(Result.Err.class);
+        ParseError error = ((Result.Err<ScoreRecord, ParseError>) result).error();
+        assertThat(error.offendingLine()).isEqualTo(csvLine);
+    }
 }
