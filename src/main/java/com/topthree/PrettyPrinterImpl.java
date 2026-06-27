d270defa8e5390eb7f0aa29ae3ac36711d038bb5
Dave <davecranesunwheel@gmail..com>
2026-06-27 18:38:23 +0100

    feat(green): PrettyPrinter.print implementation

--- src/main/java/com/topthree/PrettyPrinterImpl.java
+++ src/main/java/com/topthree/PrettyPrinterImpl.java
@@ -4,6 +4,13 @@ public class PrettyPrinterImpl implements PrettyPrinter {
 
     @Override
     public String print(ScoreRecord record) {
-        return "";
+        Player player = record.player();
+        GameEntry game = record.gameEntry();
+        return player.playerId() + "," +
+                player.playerName() + "," +
+                game.gameId() + "," +
+                game.gameName() + "," +
+                game.hoursPlayed() + "," +
+                game.normalisedScore();
     }
 }
