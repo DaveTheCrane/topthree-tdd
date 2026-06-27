3ea27f0bfd59838e0a5a3ee7e2a3255061fcf9be
Dave <davecranesunwheel@gmail..com>
2026-06-27 18:22:47 +0100

    feat(green): integer-parse guard for hours-played

--- src/main/java/com/topthree/CsvParserImpl.java
+++ src/main/java/com/topthree/CsvParserImpl.java
@@ -10,11 +10,17 @@ public class CsvParserImpl implements CsvParser {
         if (fields.length != 6) {
             return new Result.Err<>(new ParseError("Expected 6 fields but got " + fields.length, csvLine));
         }
+        int hoursPlayed;
+        try {
+            hoursPlayed = Integer.parseInt(fields[4]);
+        } catch (NumberFormatException e) {
+            return new Result.Err<>(new ParseError("Invalid integer for hours-played: " + fields[4], csvLine));
+        }
         Player player = new Player(fields[0], fields[1]);
         GameEntry gameEntry = new GameEntry(
                 fields[2],
                 fields[3],
-                Integer.parseInt(fields[4]),
+                hoursPlayed,
                 Integer.parseInt(fields[5])
         );
         return new Result.Ok<>(new ScoreRecord(player, gameEntry));
