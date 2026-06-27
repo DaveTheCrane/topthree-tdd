128c68f1fc89daa988ed64e4135fb5869f0b404f
Dave <davecranesunwheel@gmail..com>
2026-06-27 22:03:59 +0100

    feat(green): field-count validation

--- src/main/java/com/topthree/CsvParserImpl.java
+++ src/main/java/com/topthree/CsvParserImpl.java
@@ -6,7 +6,10 @@ public class CsvParserImpl implements CsvParser {
 
     @Override
     public Result<ScoreRecord, ParseError> parseLine(String csvLine) {
-        String[] fields = csvLine.split(",");
+        String[] fields = csvLine.split(",", -1);
+        if (fields.length != 6) {
+            return new Result.Err<>(new ParseError("Expected 6 fields but got " + fields.length, csvLine));
+        }
         String playerId = fields[0];
         String playerName = fields[1];
         String gameId = fields[2];
