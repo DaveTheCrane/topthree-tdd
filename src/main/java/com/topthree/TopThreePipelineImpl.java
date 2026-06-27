cb10e75877e06e2fb775af5d7a33de842aa717f8
Dave <davecranesunwheel@gmail..com>
2026-06-27 23:18:13 +0100

    feat(green): pipeline wired, empty input handled

--- src/main/java/com/topthree/TopThreePipelineImpl.java
+++ src/main/java/com/topthree/TopThreePipelineImpl.java
@@ -4,8 +4,27 @@ import java.util.List;
 
 public class TopThreePipelineImpl implements TopThreePipeline {
 
+    private final CsvParser parser = new CsvParserImpl();
+    private final ScoreAggregator aggregator = new ScoreAggregatorImpl();
+    private final LeaderboardRanker ranker = new LeaderboardRankerImpl();
+
     @Override
     public Result<RankedResult, PipelineError> run(List<String> csvLines) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        var parseResult = parser.parseLines(csvLines);
+        if (parseResult instanceof Result.Err<List<ScoreRecord>, ParseError> err) {
+            var pe = err.error();
+            return new Result.Err<>(new PipelineError(pe.message(), pe.offendingLine()));
+        }
+        var records = ((Result.Ok<List<ScoreRecord>, ParseError>) parseResult).value();
+
+        var aggregateResult = aggregator.aggregate(records);
+        if (aggregateResult instanceof Result.Err<List<PlayerAggregate>, AggregationError> err) {
+            var ae = err.error();
+            return new Result.Err<>(new PipelineError(ae.message(), ae.playerId()));
+        }
+        var aggregates = ((Result.Ok<List<PlayerAggregate>, AggregationError>) aggregateResult).value();
+
+        var rankedResult = ranker.rank(aggregates);
+        return new Result.Ok<>(rankedResult);
     }
 }
