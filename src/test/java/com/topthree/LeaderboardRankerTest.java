e5daa5d59d325b87fa2b4cadbfb68cb458c2d5cd
Dave <davecranesunwheel@gmail..com>
2026-06-27 22:57:43 +0100

    test(red): single player goes into definiteWinners

--- src/test/java/com/topthree/LeaderboardRankerTest.java
+++ src/test/java/com/topthree/LeaderboardRankerTest.java
@@ -16,4 +16,13 @@ class LeaderboardRankerTest {
 
         assertEquals(new RankedResult(List.of(), List.of()), result);
     }
+
+    @Test
+    void singlePlayerGoesIntoDefiniteWinnersWithEmptyTiedCandidates() {
+        var agg = new PlayerAggregate(new Player("p1", "Alice"), 100);
+
+        var result = ranker.rank(List.of(agg));
+
+        assertEquals(new RankedResult(List.of(agg), List.of()), result);
+    }
 }
