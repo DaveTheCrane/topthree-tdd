2c3755ef4f6b00d324f213bfbe62b307a80e4449
Dave <davecranesunwheel@gmail..com>
2026-06-28 11:05:13 +0100

    chore: project setup

---
+++ src/test/java/com/topthree/SmokeTest.java
@@ -0,0 +1,12 @@
+package com.topthree;
+
+import org.junit.jupiter.api.Test;
+
+class SmokeTest {
+
+    @Test
+    void buildToolchainIsConfiguredCorrectly() {
+        // This test exists solely to verify that the Maven build toolchain is configured
+        // and working correctly. If this test runs and passes, we can proceed with TDD.
+    }
+}
