c0c8d0bd32ba7899eceb066681b611c84a3cf2a5
Dave <davecranesunwheel@gmail..com>
2026-06-28 10:09:40 +0100

    chore: project setup

---
+++ src/test/java/SmokeTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SmokeTest {
+    @Test
+    void testBuildToolchainWorks() {
+        assertThat(true).isTrue();
+    }
+}
