a66ca5b117920e2d9e8dabcdc3d89c7e106688b5
Dave <davecranesunwheel@gmail..com>
2026-06-28 21:41:37 +0100

    chore: project setup

---
+++ src/test/java/com/topthree/SanityTest.java
@@ -0,0 +1,11 @@
+package com.topthree;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SanityTest {
+    @Test
+    void sanityCheck() {
+        assertTrue(true, "Build toolchain is working");
+    }
+}
