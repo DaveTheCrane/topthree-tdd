890e8629e4cc169b0d0975154ec8314e3326e65e
Dave Crane <davecranesunwheel@gmail.com>
2026-07-04 17:24:09 +0100

    chore: project setup

---
+++ src/test/java/EmptyTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmptyTest {
+    @Test
+    void emptyTest() {
+        assertTrue(true, "This test should always pass");
+    }
+}
